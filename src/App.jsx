7899c7fa80d42ee62263b43146aba3aaeee6f0b8
Le Hoai Nguyen Phuc <lehoainguyenphuc11b3@gmail.com>
2025-03-27 09:25:14 +0700

    Uninstall Lazyload

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { lazy, Suspense } from 'react';
-import LazyLoad from 'react-lazyload';
-const Hero = lazy(() => import('./components/hero/Hero'));
-const Services = lazy(() => import('./components/services/Services'));
-const Portfolio = lazy(() => import('./components/portfolio/Portfolio'));
-const Contact = lazy(() => import('./components/contact/Contact'));
+import { Suspense } from 'react';
+import Hero from './components/hero/Hero';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+
 
 const App = () => {
   return (
     <div className='container'>
       <Suspense fallback={<div>Loading...</div>}>
-        <LazyLoad height={100} offset={-100} >
-          <section className='#home'>
-            <Hero />
-          </section>
-        </LazyLoad>
+
+        <section className='#home'>
+          <Hero />
+        </section>
       </Suspense>
       <Suspense fallback={<div></div>}>
-        <LazyLoad height={100} offset={-100} >
-          <section className='#services'>
-            <Services />
-          </section>
-        </LazyLoad>
+
+        <section className='#services'>
+          <Services />
+        </section>
+
       </Suspense>
 
       <Suspense fallback={<div></div>}>
-        <LazyLoad height={600} offset={-100} >
-          <Portfolio />
-        </LazyLoad>
+
+        <Portfolio />
+
       </Suspense>
 
       <Suspense fallback={<div></div>}>
-        <LazyLoad height={100} offset={-100} >
-          <section className='#contact'>
-            <Contact />
-          </section>
-        </LazyLoad>
+
+        <section className='#contact'>
+          <Contact />
+        </section>
+
       </Suspense>
     </div>
   )
